a6e863f34c75ab6e66d3f319a3601bc7e82458bd
tadhg03 <tadhg_mccarthy@dlsu.edu.ph>
2019-12-09 05:30:41 +0800

    forgot to push these for semantic analysis first push

---
+++ src/Function.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Function {
+
+    private String acc = ""; //public or private
+    private String name = "";
+    private String type = "";
+    private String value = "";
+    private ArrayList<Variable> params = new ArrayList<>();
+
+    public Function(){
+
+    }
+}
---
+++ src/Variable.java
@@ -0,0 +1,73 @@
+public class Variable {
+
+    private boolean constant = false;
+    private String acc = ""; //public or private
+    private String name = "";
+    private String type = "";
+    private String value = "";
+
+    public Variable(boolean constant, String acc, String name, String type, String value){
+        this.constant = constant;
+        this.acc = acc;
+        this.name = name;
+        this.type = type;
+        this.value = value;
+    }
+
+    public Variable(String acc, String name, String type, String value){
+        this.acc = acc;
+        this.name = name;
+        this.type = type;
+        this.value = value;
+    }
+
+    public Variable(String acc, String name, String type){
+        this.acc = acc;
+        this.name = name;
+        this.type = type;
+    }
+
+    public Variable(){
+
+    }
+
+    public String getAcc() {
+        return acc;
+    }
+
+    public void setAcc(String acc) {
+        this.acc = acc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public boolean isConstant() {
+        return constant;
+    }
+
+    public void setConstant(boolean constant) {
+        this.constant = constant;
+    }
+}
